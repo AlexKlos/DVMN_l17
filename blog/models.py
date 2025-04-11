35a8638cf099217aef1b1317186e6fd0075b49a6
AlexKlos <klos@list.ru>
2025-04-11 16:33:46 +0700

    move queries into QuerySet methods

--- blog/models.py
+++ blog/models.py
@@ -6,15 +6,33 @@ from django.contrib.auth.models import User
 
 class PostQuerySet(models.QuerySet):
 
-    def year(self, year):
-        post_at_year = self.filter(published_at__year=year).order_by('published_at')
-        return post_at_year
+    def popular(self):
+        popular_post = self.annotate(likes_count=Count('likes')).order_by('-likes_count')
+
+        return popular_post
+    
+    def fetch_with_comments_count(self):
+        """
+        Данный метод можно использовать после popular() и других аннотаций,
+        что значительно ускоряет выполнение кода.
+        """
+        posts = self
+        posts_ids = [post.id for post in posts]
+        posts_with_comments = Post.objects.filter(id__in=posts_ids).annotate(comments_count=Count('comments'))
+        ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+        count_for_id = dict(ids_and_comments)
+        
+        for post in posts:
+            post.comments_count = count_for_id[post.id]
+
+        return posts
 
 
 class TagQuerySet(models.QuerySet):
 
     def popular(self):
         popular_tag = self.annotate(related_posts_count=Count('posts')).order_by('-related_posts_count')
+        
         return popular_tag
 
 
