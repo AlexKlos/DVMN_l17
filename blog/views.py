63389b54febfda08559d830cc78b1036732e7563
AlexKlos <klos@list.ru>
2025-04-21 18:57:16 +0500

    Удалите неиспользуемый код

--- blog/views.py
+++ blog/views.py
@@ -3,24 +3,6 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Comment, Post, Tag
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
-
-def serialize_post(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all().first().title,
-    }
-
-
 def serialize_post_optimized(post):
     tags = list(post.tags.all())
     first_tag = tags[0] if tags else None
@@ -35,13 +17,6 @@ def serialize_post_optimized(post):
         'slug': post.slug,
         'tags': [serialize_tag_optimized(tag) for tag in post.tags.all()],
         'first_tag_title': first_tag.title if first_tag else None,
-    }
-
-
-def serialize_tag(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
 
